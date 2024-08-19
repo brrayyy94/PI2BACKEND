743114a5941fa34405b7087064e616edf886eddb
Juan José Saavedra Realpe <juanjsaavedra52@gmail.com>
2024-08-18 19:44:46 -0500

    Now the answers are send in raw without being inside a message field

--- network/response.mjs
+++ network/response.mjs
@@ -1,8 +1,5 @@
 export const success = (res, message, status) => {
-    res.status(status || 200).send({
-        message,
-        status
-    });
+    res.status(status || 200).send(message);
 };
 
 export const error = (res, message, status, details) => {
