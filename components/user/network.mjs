c2f4189aa05e58e7735e28325501ff810cb403a8
brrayyy09 <brayanmaca94@gmail.com>
2024-08-21 21:03:35 -0500

    solucione un problema de merge

--- components/user/network.mjs
+++ components/user/network.mjs
@@ -1,20 +1,20 @@
 import { Router } from "express";
 import { success, error } from "../../network/response.mjs";
-import { add, get, update, remove, login } from "./controller.mjs";
+import { add, get, update, remove, getById, login} from "./controller.mjs";
 
 const router = Router();
 
-const controller = {add, get, update, remove, login};
+const controller = { add, get, update, remove, getById, login };
 
 // Route POST /user/addUser
 router.post('/addUser', (req, res) => {
     controller.add(req, res)
-    .then(({ status, message }) => {
-        success(res, message, status);
-    })
-    .catch(({ status, message }) => {
-        error(res, 'Error interno', status || 500, message);
-    });
+        .then(({ status, message }) => {
+            success(res, message, status);
+        })
+        .catch(({ status, message }) => {
+            error(res, 'Error interno', status || 500, message);
+        });
 });
 
 //Route POST /login
@@ -31,34 +31,45 @@ router.post('/login', (req, res) => {
 // Route GET /user/getUsersByComplex
 router.get('/getUsersByComplex', (req, res) => {
     controller.get(req, res)
-    .then(({ status, message }) => {
-        success(res, message, status);
-    })
-    .catch(({ status, message }) => {
-        error(res, 'Error interno', status || 500, message);
-    });
+        .then(({ status, message }) => {
+            success(res, message, status);
+        })
+        .catch(({ status, message }) => {
+            error(res, 'Error interno', status || 500, message);
+        });
+});
+
+// Route GET /user/getUserById
+router.get('/getUserById', (req, res) => {
+    controller.getById(req, res)
+        .then(({ status, message }) => {
+            success(res, message, status);
+        })
+        .catch(({ status, message }) => {
+            error(res, 'Error interno', status || 500, message);
+        });
 });
 
 // Route PUT /user/updateUser
 router.put('/updateUser', (req, res) => {
     controller.update(req, res)
-    .then(({ status, message }) => {
-        success(res, message, status);
-    })
-    .catch(({ status, message }) => {
-        error(res, 'Error interno', status || 500, message);
-    });
+        .then(({ status, message }) => {
+            success(res, message, status);
+        })
+        .catch(({ status, message }) => {
+            error(res, 'Error interno', status || 500, message);
+        });
 });
 
 // Route DELETE /user/deleteUser
 router.delete('/deleteUser', (req, res) => {
     controller.remove(req, res)
-    .then(({ status, message }) => {
-        success(res, message, status);
-    })
-    .catch(({ status, message }) => {
-        error(res, 'Error interno', status || 500, message);
-    });
+        .then(({ status, message }) => {
+            success(res, message, status);
+        })
+        .catch(({ status, message }) => {
+            error(res, 'Error interno', status || 500, message);
+        });
 });
 
 export { router };
