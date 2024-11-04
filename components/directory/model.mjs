c5785f7a7d4f96b24492752e986c48301c40aed0
SanCT5 <sanct1318@gmail.com>
2024-11-04 15:31:16 -0500

    FEAT: add directory schema

---
+++ components/directory/model.mjs
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const directorySchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    complexId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Complex',
+        required: true
+    },
+    service: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    hasWhatsApp: {
+        type: Boolean,
+        required: true
+    },
+    whatsAppNumber: {
+        type: String,
+        default: null
+    }
+});
+
+module.exports = mongoose.model('Directory', directorySchema);
