4da3bbbc50024a565f9c7176e82ccdd8e410a983
brrayyy09 <brayanmaca94@gmail.com>
2025-02-18 19:32:23 -0500

    Fix: revision del pipeline

--- jest.setup.mjs
+++ jest.setup.mjs
@@ -1,26 +1,22 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config();
 import {beforeAll, afterAll, jest} from '@jest/globals';
+import mongoose from 'mongoose';
 
-// Configuración global antes de todas las pruebas
+jest.mock('mongoose', () => ({
+    connect: jest.fn().mockResolvedValue(true),
+    connection: {
+        close: jest.fn().mockResolvedValue(true),
+    },
+}));
+
 beforeAll(async () => {
     // Mockea console.log y console.error antes de cualquier registro
     jest.spyOn(console, 'log').mockImplementation(() => {});
     jest.spyOn(console, 'error').mockImplementation(() => {});
-
-    // Conecta a la base de datos
-    const url = encodeURI(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`);
-    await mongoose.connect(url);
- 
+    // Mockea la conexión a la base de datos
+    await mongoose.connect('mock-url');
 });
 
-// Configuración global después de todas las pruebas
 afterAll(async () => {
-    // Cierra la conexión a la base de datos
+    // Mockea el cierre de la conexión
     await mongoose.connection.close();
-    console.log('Conexión a la base de datos cerrada'); // Este mensaje será suprimido
-    
-    // Restaura los mocks
-    jest.restoreAllMocks();
 });
