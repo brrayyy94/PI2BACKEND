91ef9be1480390ebba34ec4a7b82d60e60311bd0
brrayyy09 <brayanmaca94@gmail.com>
2025-02-18 19:25:14 -0500

    Fix: revision del pipeline

--- jest.setup.mjs
+++ jest.setup.mjs
@@ -9,17 +9,10 @@ beforeAll(async () => {
     jest.spyOn(console, 'log').mockImplementation(() => {});
     jest.spyOn(console, 'error').mockImplementation(() => {});
 
-    try {
-        // Conecta a la base de datos
-        const url = encodeURI(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`);
-        await mongoose.connect(url);
-        console.log('Conexión establecida con la base de datos'); // Este mensaje será suprimido
-    } catch (error) {
-        // Restaura console.error para ver el mensaje de error
-        jest.spyOn(console, 'error').mockRestore();
-        console.error('Error al conectar con la base de datos:', error);
-        throw error;
-    }
+    // Conecta a la base de datos
+    const url = encodeURI(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`);
+    await mongoose.connect(url);
+ 
 });
 
 // Configuración global después de todas las pruebas
